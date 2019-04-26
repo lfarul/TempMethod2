23a9732cc886fee639688a92c35e4cc6016471a4
lfarul <LFarulewski@yahoo.com>
2019-04-26 14:15:48 +0200

    Update TempMethod2Test.java

--- TempMethod2Test.java
+++ TempMethod2Test.java
@@ -1,12 +1,4 @@
-package tempmethod2;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-
 
 public class TempMethod2Test {
      
