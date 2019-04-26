cf1c835b0fc5129c3fdafaca3e42d2a56de4e1ed
lfarul <LFarulewski@yahoo.com>
2019-04-26 12:24:48 +0200

    Update TempMethod2Test.java

--- TempMethod2Test.java
+++ TempMethod2Test.java
@@ -1,1 +1,21 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+
+public class TempMethod2Test {
+     
+    @Test
+    
+    public void testTemperature(){
+        
+        TempMethod2 c = new TempMethod2();
+        
+        c.dockerBuildImage();
+        c.dockerHubPush();
+        c.isPositive(25);   
+    }
+}
