83387c2c88ec42126a4355448dd926912b36a2fd
lfarul <LFarulewski@yahoo.com>
2019-04-26 12:23:55 +0200

    Update TempMethod2.java

--- TempMethod2.java
+++ TempMethod2.java
@@ -1,42 +1,41 @@
 public class TempMethod2 {
     
-    //definicja metody isPositive wraz z argumentem Temp typu int
-    
-    static void dockerPush (){
+    void dockerBuildImage(){
         
-        System.out.println("Wygląda na to, że udało się przepchnąć obraz na chmurę Dockera :)");
-        
+        System.out.println("Jenkinsfile buduje obrazy dla Docker Hub oraz Google Cloud");
     }
     
-    static void isPositive(int Temp) {
-        
-        if (Temp > 0)
-        
-        {
-            System.out.println("Twoja temperatura to: " +  Temp + "." + " " + "Temperatura jest dodatnia.Więc mozesz ubrać szorty."); 
-        }
-        
-        else if (Temp < 0)
+    void dockerHubPush (){  
 
+        System.out.println("Wygląda na to, że Jenkinsowi udało się przepchnąć obraz na chmurę Dockera :)");
+    }
+    
+    //definicja metody isPositive wraz z argumentem Temp typu int
+    void isPositive(int Temp) {
+
+        if (Temp > 0)
+
+        {
+            System.out.println("Twoja temperatura to: " +  Temp + "." + " " + "Temperatura jest dodatnia, więc mozesz ubrać szorty."); 
+        }
+        else if (Temp < 0)
         {
             System.out.println("Twoja temperatura to: " + Temp + "." + " " + "Temperatura jest ujemna, więc ubierz się ciepło.");
         }
-        
         else 
         {
             System.out.println("Twoja temperatura to: " + Temp + "." + " " + "Temperatura wynosi 0, więc nie jest źle.");
         }
-        
     }
 
     public static void main(String[] args) {
-        // wywoluje metode isPositive nadajac argumentowi Temp wartosc 20 
-        isPositive(20);
         
-        System.out.println("Ten komentarz zawsze się wyświetli, więc no worries..");
+        // tworzę nowy obiekt "t" klasy TempMethod2 aby w statycznej metodzie Main wywołać metody niestatyczne.
         
-        dockerPush();
+        TempMethod2 t = new TempMethod2();
         
+        t.dockerBuildImage();
+        t.dockerHubPush();
+        t.isPositive(25);
     }
-    
 }
